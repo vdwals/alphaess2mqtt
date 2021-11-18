168bff06a27f4d69afe356066f1954d80dfb2138
Dennis van der Wals <dennis.vanderwals@fielddata.io>
2021-11-18 15:08:54 +0100

    complete running data api

--- src/main/java/app/controllers/RunningDataController.java
+++ src/main/java/app/controllers/RunningDataController.java
@@ -1,19 +1,15 @@
 package app.controllers;
 
 import app.models.api.RunningDataDto;
-import app.models.api.SummeryDto;
 import app.services.injections.IRunningDataService;
 import com.google.inject.Inject;
-import org.javalite.common.JsonHelper;
-import org.springframework.http.HttpStatus;
 
 /**
  * @author Stefano Crespi
  */
 public class RunningDataController extends APIController {
     
-    @Inject
-    private IRunningDataService runningDataService;
+    @Inject private IRunningDataService runningDataService;
     
     public void index() {
         RunningDataDto runningData = runningDataService.getRunningData();
@@ -35,9 +31,75 @@ public class RunningDataController extends APIController {
         respondWithJson(runningData.getPpv3());
     }
     
-    public void ppvSum() {
+    public void ppv4() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPpv4());
+    }
+    
+    public void pMeter1() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPmeter_l1());
+    }
+    
+    public void pMeter2() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPmeter_l2());
+    }
+    
+    public void pMeter3() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPmeter_l3());
+    }
+    
+    public void pMeterSum() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPmeter_l1() + runningData.getPmeter_l2() +
+                        runningData.getPmeter_l3());
+    }
+    
+    public void pMeterDc() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPmeter_dc());
+    }
+    
+    public void soc() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getSoc());
+    }
+    
+    public void pBat() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPbat());
+    }
+    
+    public void varAc() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getVarac());
+    }
+    
+    public void varDc() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getVardc());
+    }
+    
+    public void pReal1() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPreal_l1());
+    }
+    
+    public void pReal2() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPreal_l2());
+    }
+    
+    public void pReal3() {
+        RunningDataDto runningData = runningDataService.getRunningData();
+        respondWithJson(runningData.getPreal_l3());
+    }
+    
+    public void pRealSum() {
         RunningDataDto runningData = runningDataService.getRunningData();
         respondWithJson(
-                runningData.getPpv1() + runningData.getPpv2() + runningData.getPpv3());
+                runningData.getPreal_l1() + runningData.getPreal_l2() + runningData.getPreal_l3());
     }
 }
