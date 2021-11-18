a5763bb9989f6671701d1c8a1d7bce5f759d1664
Dennis van der Wals <dennis.vanderwals@fielddata.io>
2021-11-18 09:39:31 +0100

    Cleanup

--- src/main/java/app/config/AppControllerConfig.java
+++ src/main/java/app/config/AppControllerConfig.java
@@ -1,7 +1,5 @@
 package app.config;
 
-import app.controllers.CatchAllFilter;
-import app.controllers.PeopleController;
 import app.controllers.SummeryController;
 import org.javalite.activeweb.AbstractControllerConfig;
 import org.javalite.activeweb.AppContext;
@@ -10,7 +8,6 @@ import org.javalite.activeweb.controller_filters.DBConnectionFilter;
 public class AppControllerConfig extends AbstractControllerConfig {
 
     public void init(AppContext context) {
-        add(new CatchAllFilter(), new DBConnectionFilter()).to(PeopleController.class);
-        add(new CatchAllFilter(), new DBConnectionFilter()).to(SummeryController.class);
+        add(new DBConnectionFilter()).to(SummeryController.class);
     }
 }
