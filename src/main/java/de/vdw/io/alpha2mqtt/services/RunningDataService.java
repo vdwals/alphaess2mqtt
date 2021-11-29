86c1cb1d48d4794493d846f6869efaf2ab4dd508
Dennis van der Wals <dennis.vanderwals@fielddata.io>
2021-11-29 07:17:19 +0100

    Error logging enhanced

--- src/main/java/de/vdw/io/alpha2mqtt/services/RunningDataService.java
+++ src/main/java/de/vdw/io/alpha2mqtt/services/RunningDataService.java
@@ -7,12 +7,18 @@ import de.vdw.io.alpha2mqtt.models.AlphaEssLoadJob;
 import de.vdw.io.alpha2mqtt.models.api.ResponseDto;
 import de.vdw.io.alpha2mqtt.models.api.RunningDataDto;
 import de.vdw.io.alpha2mqtt.services.alpha.TokenService;
-import java.io.IOException;
-import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 import org.javalite.activejdbc.Base;
 import org.javalite.http.Get;
 import org.javalite.http.Http;
 
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Singleton
 public class RunningDataService extends AlphaService<RunningDataDto> {
 
   public RunningDataService(ObjectMapper objectMapper, TokenService tokenService) {
@@ -25,12 +31,33 @@ public class RunningDataService extends AlphaService<RunningDataDto> {
         Base.withDb(
             () -> {
               AlphaEssLoadJob dataJob = AlphaEssLoadJob.getSecondDataJob();
+              if (dataJob == null) {
+                log.error("No dataJob received from DB.");
+                return null;
+              }
+
               AlphaEssBattery battery = (AlphaEssBattery) AlphaEssBattery.findAll().limit(1).get(0);
+              if (battery == null) {
+                log.error("No battery received from DB.");
+                return null;
+              }
 
               return String.format(dataJob.getUrl(), battery.getSn());
             });
 
+    if (url == null) {
+      log.error("No url generated");
+      return null;
+    }
+
     Get dataGet = addHeader(Http.get(url), token);
+    if (dataGet.responseCode() != HttpURLConnection.HTTP_OK) {
+      log.error(
+          "Unexpected response code while receiving live data {}: {}",
+          dataGet.responseCode(),
+          dataGet.responseMessage());
+      return null;
+    }
 
     String dataResponse = dataGet.text();
 
@@ -41,10 +68,10 @@ public class RunningDataService extends AlphaService<RunningDataDto> {
       return runningDataResponseDto.getData();
 
     } catch (IOException e) {
-      e.printStackTrace();
-    }
+      log.error("Error receiving live data:", e);
 
-    return null;
+      return null;
+    }
   }
 
   @Override
