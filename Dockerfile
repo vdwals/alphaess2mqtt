a585e65f293bac3538544c2ba3cfbb6791eb2dd1
Dennis van der Wals <dennis.vanderwals@fielddata.io>
2021-12-20 09:48:48 +0100

    More debug logs added

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM drone/git as clone
 WORKDIR /hamqtt
 
 RUN git clone https://bitbucket.org/vdwals/hamqtt.git . \
-    && git checkout feature/2021.12
+    && git checkout release/3.3.0
 
 # Build application
 FROM arm32v7/maven:3-eclipse-temurin-17 as builder
--- src/main/java/de/vdw/io/alpha2mqtt/services/alpha/SettingService.java
+++ src/main/java/de/vdw/io/alpha2mqtt/services/alpha/SettingService.java
@@ -13,18 +13,17 @@ import de.vdw.io.alpha2mqtt.models.api.WallboxDto;
 import de.vdw.io.alpha2mqtt.models.api.charge.SettingDto;
 import de.vdw.io.alpha2mqtt.utils.RequestUtils;
 import de.vdw.it.hamqtt.utils.JsonUtils;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.javalite.activejdbc.Base;
 import org.javalite.common.JsonHelper;
 import org.javalite.http.Get;
 import org.javalite.http.Http;
 import org.javalite.http.Post;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Slf4j
 public class SettingService extends AlphaService<SystemDto> {
@@ -33,15 +32,50 @@ public class SettingService extends AlphaService<SystemDto> {
     super(objectMapper, tokenService);
   }
 
+  @Override
+  public long getRefreshRate() {
+    return Base.withDb(() -> AlphaEssLoadJob.getSettingsJob()
+        .getIntervalInSeconds());
+  }
+
+  private SystemDto getResponse(String token, String url) {
+    log.debug("Requesting settings from {}", url);
+
+    Get dataGet = RequestUtils
+        .addHeader(Http.get(url, (int) Constants.TIMEOUT, (int) Constants.TIMEOUT), token);
+
+    if (dataGet.responseCode() != HttpURLConnection.HTTP_OK) {
+      log.error("Unexpected response code while receiving items {}: {}",
+          dataGet.responseCode(),
+          dataGet.responseMessage());
+      return null;
+    }
+
+    String dataResponse = dataGet.text();
+
+    log.trace("Response: {}", dataResponse);
+
+    try {
+      ResponseDto<SystemDto> systemResponseDto =
+          getObjectMapper().readValue(dataResponse, new TypeReference<>() {});
+
+      log.trace("Settings response: {}", systemResponseDto);
+
+      return systemResponseDto.getData();
+
+    } catch (IOException e) {
+      log.error("Could not parse response.", e);
+      return null;
+    }
+  }
+
   public SettingDto getSettingDto() {
     log.debug("Build settings DTO.");
-    Optional<String> systemId =
-        Base.withDb(
-            () ->
-                AlphaEssBattery.findAll().stream()
-                    .map(model -> (AlphaEssBattery) model)
-                    .map(AlphaEssBattery::getSystemId)
-                    .findFirst());
+    Optional<String> systemId = Base.withDb(() -> AlphaEssBattery.findAll()
+        .stream()
+        .map(model -> (AlphaEssBattery) model)
+        .map(AlphaEssBattery::getSystemId)
+        .findFirst());
 
     if (systemId.isEmpty()) {
       log.error("No System Id available.");
@@ -68,7 +102,8 @@ public class SettingService extends AlphaService<SystemDto> {
     }
 
     // Add wallbox values
-    settingDto.setWallbox(systemData.getCharging_pile_list().get(0));
+    settingDto.setWallbox(systemData.getCharging_pile_list()
+        .get(0));
 
     // Set system id
     settingDto.setSystem_id(systemId.get());
@@ -76,125 +111,8 @@ public class SettingService extends AlphaService<SystemDto> {
     return settingDto;
   }
 
-  public SystemDto updateSetting(SettingDto settingDto) {
-    String token = tokenService.getToken();
-
-    String url =
-        Base.withDb(
-            () -> {
-              AlphaEssLoadJob alphaEssLoadJob = AlphaEssLoadJob.setSettingsJob();
-
-              if (alphaEssLoadJob == null) {
-                log.error("No Settings set job found");
-                return null;
-              }
-
-              return alphaEssLoadJob.getUrl();
-            });
-
-    String setting = JsonHelper.toJsonString(settingDto);
-
-    log.debug("Posting charging mode request.");
-    log.trace("Request: {}", setting);
-
-    Post post =
-        RequestUtils.addPostHeader(
-            Http.post(
-                url,
-                setting.getBytes(StandardCharsets.UTF_8),
-                (int) Constants.TIMEOUT,
-                (int) Constants.TIMEOUT),
-            token);
-
-    if (post.responseCode() != HttpURLConnection.HTTP_OK) {
-      log.error(
-          "Charging mode not changed. Code: {}, Message: {}",
-          post.responseCode(),
-          post.responseMessage());
-      return null;
-    }
-
-    log.debug("Settings changed to {}.", setting);
-    log.trace("Response: {}", post.text());
-
-    return getSystemSettings();
-  }
-
-  @Override
-  protected SystemDto requestNewData(String token, LocalDateTime now) {
-    log.info("Load wallbox information.");
-
-    String url = Base.withDb(() -> AlphaEssLoadJob.getSettingsJob().getUrl());
-
-    if (url == null) {
-      log.error("No Settings url available");
-      return null;
-    }
-
-    Optional<AlphaEssBattery> alphaEssBattery =
-        Base.withDb(
-            () ->
-                AlphaEssBattery.findAll().stream()
-                    .map(model -> (AlphaEssBattery) model)
-                    .findFirst());
-
-    if (alphaEssBattery.isEmpty()) {
-      log.error("No Battery found");
-      return null;
-    }
-
-    AlphaEssBattery battery = alphaEssBattery.get();
-    url = String.format(url, battery.getSystemId());
-
-    SystemDto systemResponseDto = getResponse(token, url);
-
-    if (systemResponseDto == null) {
-      log.error("No settings received.");
-      return null;
-    }
-
-    Base.withDb(
-        () -> {
-          for (WallboxDto wallboxDto : systemResponseDto.getCharging_pile_list()) {
-            AlphaEssWallbox.create(battery, wallboxDto.getChargingpile_sn());
-          }
-          return null;
-        });
-
-    return systemResponseDto;
-  }
-
-  private SystemDto getResponse(String token, String url) {
-    Get dataGet =
-        RequestUtils.addHeader(
-            Http.get(url, (int) Constants.TIMEOUT, (int) Constants.TIMEOUT), token);
-
-    if (dataGet.responseCode() != HttpURLConnection.HTTP_OK) {
-      log.error(
-          "Unexpected response code while receiving items {}: {}",
-          dataGet.responseCode(),
-          dataGet.responseMessage());
-      return null;
-    }
-
-    String dataResponse = dataGet.text();
-
-    try {
-      ResponseDto<SystemDto> systemResponseDto =
-          getObjectMapper().readValue(dataResponse, new TypeReference<>() {});
-
-      log.trace("Settings response: {}", systemResponseDto);
-
-      return systemResponseDto.getData();
-
-    } catch (IOException e) {
-      log.error("Could not parse response.", e);
-      return null;
-    }
-  }
-
   public SystemDto getSystemSettings() {
-    String token = tokenService.getToken();
+    String token = this.tokenService.getToken();
 
     if (token == null) {
       log.error("No token available");
@@ -202,15 +120,15 @@ public class SettingService extends AlphaService<SystemDto> {
     }
 
     log.info("Load system settings information.");
-    Optional<String> settingsUrl =
-        Base.withDb(
-            () -> {
-              String url = AlphaEssLoadJob.getSettingsJob().getUrl();
-              return AlphaEssBattery.findAll().stream()
-                  .map(model -> (AlphaEssBattery) model)
-                  .map(battery -> String.format(url, battery.getSystemId()))
-                  .findFirst();
-            });
+    Optional<String> settingsUrl = Base.withDb(() -> {
+      String url = AlphaEssLoadJob.getSettingsJob()
+          .getUrl();
+      return AlphaEssBattery.findAll()
+          .stream()
+          .map(model -> (AlphaEssBattery) model)
+          .map(battery -> String.format(url, battery.getSystemId()))
+          .findFirst();
+    });
 
     if (settingsUrl.isEmpty()) {
       log.error("No settings url available");
@@ -223,7 +141,83 @@ public class SettingService extends AlphaService<SystemDto> {
   }
 
   @Override
-  public long getRefreshRate() {
-    return Base.withDb(() -> AlphaEssLoadJob.getSettingsJob().getIntervalInSeconds());
+  protected SystemDto requestNewData(String token, LocalDateTime now) {
+    log.info("Load wallbox information.");
+
+    String url = Base.withDb(() -> AlphaEssLoadJob.getSettingsJob()
+        .getUrl());
+
+    if (url == null) {
+      log.error("No Settings url available");
+      return null;
+    }
+    log.debug("URL loaded");
+
+    Optional<AlphaEssBattery> alphaEssBattery = Base.withDb(() -> AlphaEssBattery.findAll()
+        .stream()
+        .map(model -> (AlphaEssBattery) model)
+        .findFirst());
+
+    if (alphaEssBattery.isEmpty()) {
+      log.error("No Battery found");
+      return null;
+    }
+    log.debug("Battery loaded");
+
+    AlphaEssBattery battery = alphaEssBattery.get();
+    url = String.format(url, battery.getSystemId());
+
+    SystemDto systemResponseDto = getResponse(token, url);
+
+    if (systemResponseDto == null) {
+      log.error("No settings received.");
+      return null;
+    }
+
+    Base.withDb(() -> {
+      for (WallboxDto wallboxDto : systemResponseDto.getCharging_pile_list()) {
+        AlphaEssWallbox.create(battery, wallboxDto.getChargingpile_sn());
+      }
+      return null;
+    });
+
+    return systemResponseDto;
+  }
+
+  public SystemDto updateSetting(SettingDto settingDto) {
+    String token = this.tokenService.getToken();
+
+    String url = Base.withDb(() -> {
+      AlphaEssLoadJob alphaEssLoadJob = AlphaEssLoadJob.setSettingsJob();
+
+      if (alphaEssLoadJob == null) {
+        log.error("No Settings set job found");
+        return null;
+      }
+
+      return alphaEssLoadJob.getUrl();
+    });
+
+    String setting = JsonHelper.toJsonString(settingDto);
+
+    log.debug("Posting charging mode request.");
+    log.trace("Request: {}", setting);
+
+    Post post = RequestUtils.addPostHeader(Http.post(url,
+        setting.getBytes(StandardCharsets.UTF_8),
+        (int) Constants.TIMEOUT,
+        (int) Constants.TIMEOUT), token);
+
+    if (post.responseCode() != HttpURLConnection.HTTP_OK) {
+      log.error("Charging mode not changed. Code: {}, Message: {}",
+          post.responseCode(),
+          post.responseMessage());
+      return null;
+    }
+
+    log.debug("Settings changed to {}.", setting);
+    log.trace("Response: {}", post.text());
+
+    return getSystemSettings();
   }
 }
