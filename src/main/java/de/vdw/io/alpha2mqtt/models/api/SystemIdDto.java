f2e218ef84c5c46bf4532ecc63ee3f226a747ee1
Dennis van der Wals <dennis@vdwals.de>
2023-05-27 15:06:31 +0200

    g

--- src/main/java/de/vdw/io/alpha2mqtt/models/api/SystemIdDto.java
+++ src/main/java/de/vdw/io/alpha2mqtt/models/api/SystemIdDto.java
@@ -2,19 +2,15 @@ package de.vdw.io.alpha2mqtt.models.api;
 
 import lombok.Value;
 import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonAnySetter;
-
 import lombok.Singular;
 
 @Value
 public class SystemIdDto {
   String system_id, sys_sn, bakbox_ver;
   Double icdc_id;
-  
+
   @JsonAnySetter
-
   @Singular("any")
-
-Map<String, String> properties;
+  Map<String, String> properties;
 }
